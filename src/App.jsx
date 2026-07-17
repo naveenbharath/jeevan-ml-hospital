feb1096bea1da7029b0ea674502410f801430d96
NAVEEN BHARATH D <naveenbharath2747@gmail.com>
2026-07-18 00:33:32 +0530

    Changes for the site visiblity by google verify console

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import AppRoutes from './routes/AppRoutes.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <Analytics />
     </BrowserRouter>
   )
 }
